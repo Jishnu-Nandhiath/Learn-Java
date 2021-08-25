0014d285e13577694b9a29e4715421c054a77e25
Jishnu-Nandhiath <jishnun789@gmail.com>
2021-08-25 15:02:40 +0530

    Added documentation to all base java operation files

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+// The main reason for the class and file name 
+// Is to make the job of the complier easy to check which class it needs to run,  in the whole list of the Java classes
+
+
 public class HelloWorld
 {
     public static void main(String[] args)
--- TakeInputs.java
+++ TakeInputs.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 public class TakeInputs {
     public static void main(String[] args) {
         System.out.println("Let's take some inputs from you first");
-        Scanner input = new Scanner(System.in);
-        System.out.println("What's your name brother ?");
-        String userInput = input.next();
-        System.out.println(userInput);
+        Scanner input = new Scanner(System.in); 
+        // A variable of type scanner is created, to scan the console for inputs.
+
+        System.out.println("Lets take the entire next line as the input ?");
+        String userInput = input.nextLine();
+        // This will consider the entire next line as a string, including white spaces. 
+        System.out.println("The user input in the first line is " + userInput); 
+   
+
+
         int age = input.nextInt();
+        //Scans the console for integer inputs. 
         System.out.println(age);
+
+        //scanner.nextdatatye() is used to take input of the appropriate datatype, in the variable
+        //Wrong type of data entered in the console will cause the compiler to throw run time errors
+
+
         input.close();
+        //Not closing the scanner will result in memory leakage. 
+        //Even if the garbage collection is done by the JVM, Its preferred to explicitly close the scanner
+        //for better code optimization
     }
 }
