af09a98e7700e453f6997ad61f607af788a7d17f
Jishnu-Nandhiath <jishnun789@gmail.com>
2021-09-21 15:39:09 +0530

    Added Java practice files in Linkedin Java Course

---
+++ Java Basics/Java Essential Training Linkedin Course/1 Syntax and Structure/HelloWorld/src/com/practice/java/Main.java
@@ -0,0 +1,14 @@
+package com.practice.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Java");
+
+        for(String s: args){
+            System.out.println(s);
+        }
+
+    }
+}
